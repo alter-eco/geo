22fe27d977976008058e317a9e0345058876db52
Laurence Dorman <l.dorman@alternatives-economiques.fr>
2018-03-05 11:31:51 +0100

    remove: topojson from src, build script added

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var jsonminify = require('gulp-jsonminify');
-
-gulp.task('default', function () {
-  gulp.src(['./src/*.json', './src/*.geojson'])
-      .pipe(jsonminify())
-      .pipe(gulp.dest('dist'));
-});
